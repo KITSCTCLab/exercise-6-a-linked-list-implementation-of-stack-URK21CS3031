5e830637e630240a6c2f71e969d32d5c0ac7ea86
URK21CS3031 <110385617+URK21CS3031@users.noreply.github.com>
2022-09-10 17:27:20 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,31 +1,49 @@
-lass Node:
-  def _init_(self, data):
+class Node:
+
+  def __init__(self, data):
     self.data = data
     self.next = None
 
+
 class Stack:
-  def _init_(self):
-    self.head = None
+  def __init__(self):
+
+    self.top = None
 
   def push(self, data) -> None:
-    nn = Node(data)
-    nn.next = self.head
-    self.head = nn
 
+      if self.top is None:
+        new_node = Node(data)
+        self.top = new_node
+      else:
+        new_node = Node(data)
+        new_node.next = self.top
+        self.top = new_node
   def pop(self) -> None:
-    if(self.head!=None):
-      temp = self.head
-      self.head = temp.next
+
+      if self.top != None:
+        if self.top.next == None:
+          self.top = None
+        else:
+          self.top = self.top.next
+
+
   def status(self):
-    if(self.head  != None):
-      temp = self.head
-      while temp.next!= None:
-        print(temp.data, end = "")
-        print("=>", end = "")
+    """
+    It prints all the elements of stack.
+    """
+    temp = self.top
+    if temp == None:
+      print(temp)
+    if temp is not None:
+      while temp.next != None:
+        print(temp.data,end="=>")
         temp = temp.next
-      print(temp.data, end = "=>")
-      
-    print("None")
+      if temp.next == None:
+        print(temp.data,end="=>")
+        print(None)
+
+
 
 # Do not change the following code
 stack = Stack()
